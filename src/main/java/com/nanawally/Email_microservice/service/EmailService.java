cea852c6b0ad1b9ff223180f1d28a1cbba41debd
liluppis <lisa.upp@hotmail.com>
2025-12-03 12:51:20 +0100

    add: logic for sending emails in service
    update: app.rops to use env variables

--- src/main/java/com/nanawally/Email_microservice/service/EmailService.java
+++ src/main/java/com/nanawally/Email_microservice/service/EmailService.java
@@ -2,16 +2,38 @@ package com.nanawally.Email_microservice.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
     private final Logger log = LoggerFactory.getLogger(EmailService.class);
+    private final JavaMailSender mailSender;
 
-    private static final String ADMIN_EMAIL = "lisa.upperud@stud.sti.se";
+    @Value("${SMTP_USERNAME}")
+    private String adminEmail;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     public void sendAdminNotification(String newUserUsername) {
-        log.info("Sending email to admin: {}, new user registered for {}", ADMIN_EMAIL, newUserUsername);
+        log.info("Sending email to admin: {}, new user registered for {}", adminEmail, newUserUsername);
+
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(adminEmail);
+            message.setSubject("New user has been registered");
+            message.setText("A new user has registered with username: " +  newUserUsername);
+
+            mailSender.send(message);
+
+            log.info("Email sent to {}", adminEmail);
+        } catch (Exception e) {
+            log.error("Error sending email to admin: {}", adminEmail, e);
+        }
     }
 }
