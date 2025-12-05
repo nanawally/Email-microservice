31e0df8412e1ff763f96609d4585c2374baf9a9a
Anna Wallström <anoli89@hotmail.com>
2025-12-05 21:59:42 +0100

    Add: server.address to app.properties

--- src/main/java/com/nanawally/Email_microservice/config/RabbitConfig.java
+++ src/main/java/com/nanawally/Email_microservice/config/RabbitConfig.java
@@ -3,7 +3,6 @@ package com.nanawally.Email_microservice.config;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 @EnableRabbit
 public class RabbitConfig {
